4c34912a258c7d0399b7bbf52acaae9a63df33e7
Erik Wijmans <ewijmans2@gmail.com>
2017-12-06 21:21:40 -0500

    Uses HS if you have it

---
+++ zsh/init_plugin_manager.zsh
@@ -0,0 +1,15 @@
+if (( $+commands[runghc] )); then
+	. ~/.zsh/antigen-hs/init.zsh
+else
+	. ~/.zsh/antigen/antigen.zsh
+
+	antigen bundle Tarrasch/zsh-functional
+	antigen bundle Tarrasch/zsh-bd
+	antigen bundle zsh-users/zsh-syntax-highlighting
+	antigen bundle zsh-users/zsh-history-substring-search
+	antigen bundle zsh-users/zsh-completions
+	antigen bundle frmendes/geometry
+	antigen bundle Tarrasch/zsh-command-not-found
+	antigen bundle Tarrasch/zsh-mcd
+	antigen apply
+fi
